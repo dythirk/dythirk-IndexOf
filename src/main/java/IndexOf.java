0fb8d490c9fdec0a91a20eba7be0efdedd6a1f9b
Thai Vo <dythirk@gmail.com>
2024-08-15 16:37:28 +0000

    Final IndexOf

--- src/main/java/IndexOf.java
+++ src/main/java/IndexOf.java
@@ -14,7 +14,7 @@ public class IndexOf {
         int value = -1;
         for (int i = 0; i < arr.length; i++){
             if (n == arr[i]){
-                    value = n;
+                    value = i;
                     return value;
                 }        
         }
